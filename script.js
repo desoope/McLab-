e10877a01bdeff56226fec528218568c38207e16
desoope <desoope@gmail.com>
2026-01-15 23:32:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const WHITELIST = {
     'Sashaiolh': { role: 'НеАдмин', isAdmin: false },
     'BobrKu': { role: 'Модератор', isAdmin: false },
     'Yamix': { role: 'Модератор', isAdmin: false },
-    'GintaRus': { role: 'Дизайнер', isAdmin: false },
+    'GintaRus': { role: 'Дизайнер', isAdmin: true },
     'MrMaiK': { role: 'Строитель', isAdmin: false },
     '_artifev_': { role: 'Модератор', isAdmin: false }
 };
